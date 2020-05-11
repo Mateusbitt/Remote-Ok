08c4c12f04b2df5853e3261297265ae106e6e79b
Bittencourt <Bittencourt@DESKTOP-JE72OPT>
2020-05-11 18:35:48 -0300

    Trabalho finalizado para entrega 11/05/2020 18:35

---
+++ src/remoteokdesktop/Application.java
@@ -0,0 +1,10 @@
+package remoteokdesktop;
+
+import remoteokdesktop.gui.ListFrame;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ListFrame listFrame = new ListFrame("teste@teste.com.br");
+    }
+}
